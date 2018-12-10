a8bc276e0facbc8f32de6b8a3f580bb72aee49ad
Torsten Link <torstenlink@gmx.ce>
2018-12-10 14:17:56 +0100

    fix(getter in index ): add getLogAdapter functions

--- src/index.js
+++ src/index.js
@@ -2,4 +2,41 @@ import LogAdapterConsole from './LogAdapterConsole'
 import LogAdapterMemory from './LogAdapterMemory'
 import LogAdapterFile from './LogAdapterFile'
 
-export { LogAdapterConsole, LogAdapterMemory, LogAdapterFile }
+
+// Stores the logger instance
+let logAdapterConsole
+let logAdapterMemory
+let logAdapterFile
+
+/**
+ * returns the logAdapter
+ */
+function getLogAdapterConsole(opts) {
+  if (logAdapterConsole === undefined) {
+    logAdapterConsole = new LogAdapterConsole(opts)
+  }
+  return logAdapterConsole
+}
+
+/**
+ * returns the logAdapter
+ */
+function getLogAdapterMemory(opts) {
+  if (logAdapterMemory === undefined) {
+    logAdapterMemory = new LogAdapterMemory(opts)
+  }
+  return logAdapterMemory
+}
+
+/**
+ * returns the logAdapter
+ */
+function getLogAdapterFile(opts) {
+  if (logAdapterFile === undefined) {
+    logAdapterFile = new LogAdapterFile(opts)
+  }
+  return logAdapterFile
+}
+
+
+export { LogAdapterConsole, LogAdapterMemory, LogAdapterFile, getLogAdapterConsole, getLogAdapterMemory, getLogAdapterFile }
